a74ea638a2639c883a76a7a735b575a5f832d402
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2025-01-20 20:19:22 +0530

    favicon update, changed from typescript to react

--- Demo1/app/(tabs)/index.jsx
+++ Demo1/app/(tabs)/index.jsx
@@ -1,10 +1,14 @@
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
 
+import backgroundImage from '../../assets/images/Test_image_2.png'
+
 const app = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('../../assets/images/Doc-Assist_Pro_Logo.png')} style={{width: '100%', height: '100%'}}>
+      <ImageBackground source={backgroundImage}
+      resizeMode='cover'
+       style={styles.bgimage}>
         <Text style={styles.text}>Doc-Assist Pro</Text>
       </ImageBackground>
     </View> 
@@ -20,10 +24,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  bgimage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0.5
+  },
   text: {
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
     textAlign: 'center', 
+    backgroundColor: 'rgba(0,0,0,0.5)',
   }
 })
